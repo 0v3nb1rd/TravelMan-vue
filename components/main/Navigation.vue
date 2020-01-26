26c1536525926d9bc07cb39b5cbcdd60de6e14b2
0v3nb1rd <0v3nb1rd@gmail.com>
2020-01-26 19:59:48 +0200

    add navigation menu

--- components/main/Navigation.vue
+++ components/main/Navigation.vue
@@ -1,6 +1,55 @@
 <template>
   <div>
-    <v-navigation-drawer v-model="drawer" app>
+    <v-app-bar app>
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        class="hidden-lg-and-up"
+      />
+      <v-toolbar-title class="logo logo-letter">
+        <nuxt-link to="/">Travel-<span>Man</span> </nuxt-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <!-- NAVBAR FOR MEDIUM AND UP SCREEN SIZES -->
+      <v-toolbar-items class="hidden-md-and-down">
+        <v-btn flat to="/services">
+          <v-icon class="primary--text navbar-icons"
+            >fas fa-hands-helping</v-icon
+          >&nbsp;
+          <span class="nav-text primary--text">Путишествие</span>
+        </v-btn>
+        <v-btn flat to="/store">
+          <v-icon class="primary--text navbar-icons">fas fa-store</v-icon>&nbsp;
+          <span class="nav-text primary--text">Технологии</span>
+        </v-btn>
+        <v-btn flat to="/sponsors">
+          <v-icon class="primary--text navbar-icons">fas fa-address-card</v-icon
+          >&nbsp;
+          <span class="nav-text primary--text">Психология</span>
+        </v-btn>
+        <v-btn flat to="/events">
+          <v-icon class="primary--text navbar-icons">fas fa-calendar-alt</v-icon
+          >&nbsp;
+          <span class="nav-text primary--text">Здоровье</span>
+        </v-btn>
+        <v-btn flat to="/about">
+          <v-icon class="primary--text navbar-icons">fas fa-user-circle</v-icon
+          >&nbsp;
+          <span class="nav-text primary--text">Про нас</span>
+        </v-btn>
+      </v-toolbar-items>
+
+      <!-- NAVBAR FOR SMALL AND EXTRA SMALL SCREEN SIZES -->
+    </v-app-bar>
+
+    <v-navigation-drawer
+      clipped
+      temporary
+      v-model="drawer"
+      class="hidden-lg-and-up"
+      fixed
+      app
+    >
       <v-list dense>
         <v-list-item link>
           <v-list-item-action>
@@ -20,11 +69,6 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-
-    <v-app-bar app color="indigo" dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
   </div>
 </template>
 
