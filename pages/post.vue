64a84b19b9022ddc3d6d20685ea0bc25d5508933
0v3nb1rd <0v3nb1rd@gmail.com>
2020-01-29 19:57:13 +0200

    add post page

---
+++ pages/post.vue
@@ -0,0 +1,86 @@
+<template>
+  <!--Card content-->
+  <v-card
+    class="text-center mx-auto justify-center"
+    style="margin-top: -64px;"
+    elevation="5"
+    max-width="90%"
+  >
+    <!--Title-->
+    <v-card-actions class="justify-center buttons__nav">
+      <v-btn color="error" large>Primary button</v-btn>
+      <v-btn color="success" large>Secondary button</v-btn>
+      <v-btn color="primary" large>Default button</v-btn>
+    </v-card-actions>
+
+    <v-divider></v-divider>
+
+    <h4 class="card-title">
+      <strong>My adventure</strong>
+    </h4>
+    <h5 class="blue-text">
+      <strong>Photography</strong>
+    </h5>
+
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+    <h3>Hello</h3>
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+    <p class="card-text">
+      Sed ut perspiciatis unde omnis iste natus sit voluptatem accusantium doloremque
+      laudantium, totam rem aperiam.
+    </p>
+  </v-card>
+  <!--/.Card content-->
+</template>
+
+<style lang="scss" scoped>
+.card-title {
+  font-size: 36px;
+  font-weight: 500;
+}
+.card-text {
+  font-size: 19px;
+}
+
+.buttons__nav {
+  padding: 10px 0 10px 0;
+}
+</style>
