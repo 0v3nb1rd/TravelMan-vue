be50c71c7d3aca6e1a4205dc2d6098efd57b92c3
0v3nb1rd <0v3nb1rd@gmail.com>
2020-02-05 18:29:08 -0800

    add create post page

--- pages/admin/post/_id.vue
+++ pages/admin/post/_id.vue
@@ -1,30 +1,123 @@
 <template>
-  <v-breadcrumbs divider=">" :items="items">
-    <template v-slot:item="{ item }">
-      <v-breadcrumbs-item :href="item.href" :disabled="item.disabled">{{ item.text.toUpperCase() }}</v-breadcrumbs-item>
-    </template>
-  </v-breadcrumbs>
+  <div class="page-wrap">
+    <v-snackbar top v-model="snackbar">{{ message }}</v-snackbar>
+
+    <v-breadcrumbs class="mb-2" divider=">" :items="breadcrumbs">
+      <template v-slot:item="{ item }">
+        <v-breadcrumbs-item
+          :href="item.href"
+          :disabled="item.disabled"
+        >{{ item.text || post.title }}</v-breadcrumbs-item>
+      </template>
+    </v-breadcrumbs>
+
+    <v-form class="ml-5 page__form" v-model="valid" ref="form">
+      <v-textarea
+        label="Text in format .md or .html"
+        v-model.trim="text"
+        :rules="textRules"
+        required
+        outlined
+        no-resize
+        rows="10"
+      ></v-textarea>
+      <v-btn
+        @click="validate"
+        rounded
+        :disabled="!valid"
+        color="primary"
+        :loading="loading"
+      >Обновить</v-btn>
+      <span class="post__text">
+        <v-chip>
+          <v-icon left small>mdi-eye</v-icon>
+          {{ post.views }}
+        </v-chip>
+        <v-chip class="ml-3">
+          <v-icon left small>mdi-clock</v-icon>
+          {{ new Date(post.date).toLocaleString() }}
+        </v-chip>
+      </span>
+    </v-form>
+  </div>
 </template>
 
 <script>
 export default {
   layout: 'admin',
   middleware: ['admin-auth'],
+  head() {
+    return {
+      title: `Пост || ${this.post.title}`
+    }
+  },
+  validate({ params }) {
+    return Boolean(params.id)
+  },
+  async asyncData({ store, params }) {
+    const post = await store.dispatch('post/fetchAdminById', params.id)
+    // console.log({ post })
+    return { post }
+  },
   data: () => ({
-    items: [
+    loading: false,
+    valid: true,
+    snackbar: false,
+    message: '',
+
+    text: '',
+    textRules: [v => !!v || "Text can't be empty"],
+
+    breadcrumbs: [
       {
         text: 'Посты',
         disabled: false,
-        href: '/admin/list'
+        href: '/admin/list',
+        exact: true
       },
       {
-        text: '$route.params.id',
+        text: '',
         disabled: true
       }
     ]
-  })
+  }),
+  methods: {
+    async validate() {
+      if (this.$refs.form.validate()) {
+        this.loading = true
+        const formData = {
+          id: this.post._id,
+          text: this.text
+        }
+        try {
+          await this.$store.dispatch('post/update', formData)
+          this.message = 'Everything is OK :)'
+          this.snackbar = true
+          this.loading = false
+        } catch (e) {
+          console.log(e)
+          this.loading = false
+        }
+      }
+      // this.$refs.form.validate(valid => {
+      //   console.log('object')
+      //   if (valid) {
+      //     console.log('valid')
+      //   } else {
+      //     console.log('not valid')
+      //   }
+      // })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.page-wrap {
+  width: 600px;
+}
+.post__text {
+  text-align: right;
+  float: right;
+}
 </style>
--- store/post.js
+++ store/post.js
@@ -1,3 +1,20 @@
+const posts = [
+  {
+    _id: 'id1',
+    title: 'Post 1',
+    views: 0,
+    comments: [1, 2],
+    date: new Date(new Date()).toLocaleString()
+  },
+  {
+    _id: 'id2',
+    title: 'Post 2',
+    views: 552,
+    comments: [1, 2],
+    date: new Date().toLocaleString()
+  }
+]
+
 export const state = () => ({})
 
 export const mutations = {}
@@ -6,27 +23,22 @@ export const actions = {
   async fetchAdmin() {
     return await new Promise(resolve => {
       setTimeout(() => {
-        resolve([
-          {
-            _id: Math.round(Math.random() * 1000),
-            title: 'Post 1',
-            views: 0,
-            comments: [1, 2],
-            date: new Date(new Date()).toLocaleString()
-          },
-          {
-            _id: Math.round(Math.random() * 1000),
-            title: 'Post 2',
-            views: 552,
-            comments: [1, 2],
-            date: new Date().toLocaleString()
-          }
-        ])
+        resolve(posts)
       }, 1000)
     })
   },
   remove({}, id) {
     console.log(id)
+  },
+  async fetchAdminById({}, id) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve(posts.find(p => p._id === id))
+      }, 1000)
+    })
+  },
+  update({}, formData) {
+    console.log('Store Ok', formData)
   }
 }
 
