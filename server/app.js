8fdf9aa528821e968453efe91f256451358e75df
0v3nb1rd <0v3nb1rd@gmail.com>
2020-02-13 16:32:30 -0800

    fixing error

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/api/auth', authRouts)
-app.use('api/post', postRoutes)
+app.use('/api/post', postRoutes)
 app.use('/api/comment', commetsRoutes)
 
 module.exports = app
